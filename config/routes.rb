c8b2ee85e0efdc75c2853c7abab9cafe17b0da73
EC2 Default User <d.h.19890404@gmail.com>
2019-01-04 12:20:30 +0000

    kadai

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root to: "toppages#index"
+  get "signup", to: "users#new"
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
+  get "rankings/want", to: "rankings#want"
+  get "rankings/have", to: "rankings#have"
+  
+  resources :users, only: [:show, :new, :create]
+  
+  resources :items, only: [:new, :show]
+  resources :ownerships, only: [:create, :destroy]
+end
